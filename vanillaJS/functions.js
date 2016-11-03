78b9f3235ecb1a5a614566834769f8483939fe7f
imadx <ahtimadhusanka@gmail.com>
2016-11-03 14:55:14 +0530

    initial commit

---
+++ vanillaJS/functions.js
@@ -0,0 +1,14 @@
+var x = function (a, b) {return a * b};
+
+console.log( "output from the function x(3,4) → " + x(3,4) );
+
+
+var output = "output from the function x(3,4) → " + x(3,4);
+console.log( output );
+
+function sayHello(name){
+	if (typeof name !== 'undefined') {
+		return "Hello, " + name + "!";
+	}
+	return "Hello, World!";
+}
